f458cc5867e04acca22bc6b373a986ef76ce1620
giovanedc <163017837+Giovvane@users.noreply.github.com>
2026-04-13 19:21:25 -0300

    Bean adicionado

--- src/br/com/fiap/bean/Aluno.java
+++ src/br/com/fiap/bean/Aluno.java
@@ -1,5 +1,44 @@
 package br.com.fiap.bean;
 
+import java.time.LocalDate;
+
 public class Aluno {
+    //Atributos
+    private int registroMatricula;
+    private String nomeCompleto;
+    private LocalDate dataDeNascimento;
+
+    //Construtor vazio
+    public Aluno(){
+
+    }
+
+    //Construtor com parâmetros
+    public Aluno(int registroMatricula, String nomeCompleto, LocalDate dataDeNascimento) {
+        this.registroMatricula = registroMatricula;
+        this.nomeCompleto = nomeCompleto;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    //Métodos Getters e Setters
+    public int getRegistroMatricula() {
+        return registroMatricula;
+    }
+    public void setRegistroMatricula(int registroMatricula) {
+        this.registroMatricula = registroMatricula;
+    }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    //Métodos
 
 }
